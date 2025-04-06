c6fc06348590c09ff512ab39e49931f6828b038e
Leandro Santos <jose.martins@accountfy.com>
2025-04-05 22:17:36 -0300

    test: add coverage threshold and output directory to Jest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,24 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src/tests'],
   clearMocks: true,
-}
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/main.ts',
+    '!src/**/index.ts',
+    '!src/**/container.ts',
+    '!src/**/logger/**',
+    '!src/tests/**',
+  ],
+  coverageDirectory: 'src/tests/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
+
+
